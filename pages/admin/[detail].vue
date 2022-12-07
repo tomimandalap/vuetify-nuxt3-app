847075229e923fb17c45d2fb89ae9734da1861e7
tomimandalap <tomi.mandala@privyid.tech>
2022-12-07 12:56:33 +0700

    feat: route parameters

---
+++ pages/admin/[detail].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Detail {{ name }}</h1>
+  </div>
+</template>
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+definePageMeta({
+  layout: 'dashboard',
+})
+
+const route = useRoute()
+const name = ref('')
+
+onMounted(() => {
+  name.value = route.params.detail
+})
+</script>
